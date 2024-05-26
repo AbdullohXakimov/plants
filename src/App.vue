f8ba66c04c897d083cf4eb81bb9cc936e6ab891a
AbdullohXakimov <Abdullohh745@gmail.com>
2024-05-26 18:38:07 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <script>
+import Header from "./components/Header.vue"
+import Intro from "./components/Intro.vue"
+import Intro2 from "./components/Intro2.vue"
+import AfterIntro from "./components/AfterIntro.vue"
+import Blog from "./components/Blog.vue"
+
 export default {
+  components: {
+    Header,
+    Intro,
+    Intro2,
+    AfterIntro,
+    Blog
+  },
   data() {
     return {
       message: "Hello Vue js",
@@ -15,28 +28,30 @@ export default {
       this.count++;
     },
 
-    getInput () {
-      this.users.push(this.name)
-    }
+    getInput() {
+      this.users.push(this.name);
+    },
   },
-
 };
 </script>
 
 <template>
-  <div>
-
-    <input type="text" v-model="name" />
-    <button @click="getInput">Ok</button>
-    <ul>
-      <li v-for="item in users">{{item}}</li>
-    </ul>
+  <Header />
+  <Intro/>
+  <Intro2/>
+  <div class="container flex justify-end gap-1 my-16">
+    <button class="border rounded-[4px] border-[#E5E5E5] py-1 px-3 hover:bg-[#46A358] hover:text-white" >1</button>
+    <button class="border rounded-[4px] border-[#E5E5E5] py-1 px-3 hover:bg-[#46A358] hover:text-white">2</button>
+    <button class="border rounded-[4px] border-[#E5E5E5] py-1 px-3 hover:bg-[#46A358] hover:text-white">3</button>
+    <button class="border rounded-[4px] border-[#E5E5E5] py-1 px-3 hover:bg-[#46A358] hover:text-white">4</button>
+    <button class="border rounded-[4px] border-[#E5E5E5] py-1 px-3 hover:bg-[#46A358] hover:text-white"><img src="../public/down.png" alt=""></button>
   </div>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
+
+  <AfterIntro/>
+  <Blog/>
 </template>
+
+
 <style scoped>
-.active {
-  font-size: 30px;
-  color: red;
-}
+
 </style>
